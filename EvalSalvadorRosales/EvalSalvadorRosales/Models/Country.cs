109d3b08fbeac1c0d66c0a16eb95c35779b48a27
Salvador Rosales Pelleschi <a2018114271@estudiantes.upsa.edu.bo>
2020-09-18 22:22:55 -0400

    MVC

---
+++ EvalSalvadorRosales/EvalSalvadorRosales/Models/Country.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EvalSalvadorRosales.Models
+{
+    public enum CurrType { 
+        code,
+        name,
+        symbol
+    }
+    public class Country
+    {
+        [Key]
+        [Required(ErrorMessage = "Nombre de pais obligatorio")]
+        [StringLength(50,ErrorMessage="Largo Maximo 50 caracteres")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Nombre de capital obligatorio")]
+        [StringLength(50, ErrorMessage = "Largo Maximo 50 caracteres")]
+        public string capital { get; set; }
+        
+        [Required(ErrorMessage = "Cantidad de Poblacion obligatoria")]
+        
+        public int population { get; set; }
+        
+        [Required]
+        public float latlng { get; set; }
+        
+        [Required(ErrorMessage = "Zona Horaria obligatoria")]
+        [RegularExpression("UTC-",ErrorMessage ="Expresion no valida")]
+        public string timezones { get; set; }
+        
+        [Required(ErrorMessage = "Moneda obligatoria")]
+        [StringLength(30, ErrorMessage = "Largo Maximo 30 caracteres")]
+        public CurrType currencies { get; set; }
+       
+        [Required(ErrorMessage = "Nombre de pais obligatorio")]
+        [StringLength(50, ErrorMessage = "Largo Maximo 50 caracteres")]
+        public string Flag { get; set; }
+
+    }
+}
---
+++ EvalSalvadorRosales/EvalSalvadorRosales/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EvalSalvadorRosales.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
